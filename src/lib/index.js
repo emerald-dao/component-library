ebae3b994369cceb57bbcd401081df384391d703
mateoroldos <mateoroldos19@gmail.com>
2022-09-13 10:10:41 -0300

    feat: Base TransactionModal component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,4 @@
 // Reexport your entry components here
 export { default as Footer } from './layout/Footer.svelte';
+export { default as Header } from './layout/Header.svelte';
+export { default as TransactionModal } from './TransactionModal.svelte';
