16056d943849e74f6399039bfad415c7d1d9fd46
mateoroldos <mateoroldos19@gmail.com>
2022-09-13 14:29:45 -0300

    feat: Add Storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	stories: [
+		'../src/**/*.stories.mdx',
+		'../src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+		'../src/lib/**/*.stories.svelte'
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: '@storybook/svelte',
+	core: {
+		builder: '@storybook/builder-vite'
+	},
+	svelteOptions: {
+		// preprocess: require('../svelte.config.js').preprocess
+	},
+	features: {
+		storyStoreV7: false
+	}
+};
---
+++ src/lib/DiscordInvite/DiscordInvite.stories.svelte
@@ -0,0 +1,13 @@
+
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import DiscordInvite from './DiscordInvite.svelte';
+</script>
+
+<Meta title="DiscordInvite" component={DiscordInvite}/>
+
+<Template let:args>
+  <DiscordInvite {...args}/>
+</Template>
+
+<Story name="Primary" args={{}}/>
