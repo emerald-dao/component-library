4ce5e711e5a865e9065ebdcb25db124477fec163
mateoroldos <mateoroldos19@gmail.com>
2022-09-14 18:59:13 -0300

    chore: Fix storybook integration to get $app alias

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,7 @@
+const preprocess = require('svelte-preprocess');
+const path = require('path');
+const { loadConfigFromFile, mergeConfig } = require('vite');
+
 module.exports = {
 	stories: [
 		'../src/**/*.stories.mdx',
@@ -20,5 +24,20 @@ module.exports = {
 	features: {
 		storyStoreV7: false
 	},
-	staticDirs: ['../static']
+	staticDirs: ['../static'],
+	async viteFinal(config, { configType }) {
+		const { config: userConfig } = await loadConfigFromFile(
+			path.resolve(__dirname, '../vite.config.js')
+		);
+		// Remove Svelte plugins that would duplicate those added by the Storybook plugin
+		const plugins = userConfig.plugins
+			.flat(1)
+			.filter(
+				(p) => !p.name.startsWith('vite-plugin-svelte') || p.name === 'vite-plugin-svelte-kit'
+			);
+		return mergeConfig(config, {
+			...userConfig,
+			plugins
+		});
+	}
 };
