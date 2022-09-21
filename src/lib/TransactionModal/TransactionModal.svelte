aeb8c639406a870181efb92b563ca3d56fb5c966
mateoroldos <mateoroldos19@gmail.com>
2022-09-21 17:04:50 -0300

    feat: Add optional border to TransactionModal

--- src/lib/TransactionModal/TransactionModal.svelte
+++ src/lib/TransactionModal/TransactionModal.svelte
@@ -1,14 +1,14 @@
 <script type="ts">
-  import { fly } from "svelte/transition";
-  import TransactionModalMessage from "./TransactionModalMessage.svelte";
-  import { backInOut } from 'svelte/easing';
-  import { Column } from '@mateoroldos/svelte.bones'
+	import { fly } from 'svelte/transition';
+	import TransactionModalMessage from './TransactionModalMessage.svelte';
+	import { backInOut } from 'svelte/easing';
+	import { Column } from '@mateoroldos/svelte.bones';
 
-  let duration = 2000;
-  let easing = backInOut;
-  let options = {duration, easing, times: 1}
-  function spin(node, options) {
-		const {times = 1} = options;
+	let duration = 2000;
+	let easing = backInOut;
+	let options = { duration, easing, times: 1 };
+	function spin(node, options) {
+		const { times = 1 } = options;
 		return {
 			...options,
 			// The value of t passed to the css method
@@ -17,29 +17,32 @@
 			css(t) {
 				// Svelte takes care of applying the easing function.
 				const degrees = 100 * times; // through which to spin
-				return `transform: translate(${(t - 1) * -10}px) rotate(${(t-1) * degrees}deg); opacity: ${1 * t};`;
+				return `transform: translate(${(t - 1) * -10}px) rotate(${
+					(t - 1) * degrees
+				}deg); opacity: ${1 * t};`;
 			}
 		};
 	}
 
-  interface FlowTransaction {
-    blockId: string,
-    errorMessage: string,
-    events: {}[],
-    status: number | "",
-    statusCode: number | "",
-    statusString: string,
-  }
+	interface FlowTransaction {
+		blockId: string;
+		errorMessage: string;
+		events: {}[];
+		status: number | '';
+		statusCode: number | '';
+		statusString: string;
+	}
 
-  export let transactionLogo = "/flow-logo.png"
-  export let dappLogo = "/flog-logo.png"
-  export let transactionName = "Flow"
-  export let transactionStatus: FlowTransaction;
-  export let transactionInProgress = false;
+	export let transactionLogo = '/flow-logo.png';
+	export let dappLogo = '/flog-logo.png';
+	export let transactionName = 'Flow';
+	export let transactionStatus: FlowTransaction;
+	export let transactionInProgress = false;
+	export let border = false;
 </script>
 
 {#if transactionInProgress}
-	<article transition:fly={{ x: 100, duration: 800 }}>
+	<article transition:fly={{ x: 100, duration: 800 }} class:border>
 		<Column gap="medium">
 			<Column gap="small">
 				<h6>{transactionName}</h6>
@@ -123,100 +126,104 @@
 {/if}
 
 <style type="scss">
-  article {
-    position: fixed;
-    top: 90px;
-    right: 20px;
-    background-color: var(--clr-primary-main);
-    box-shadow: 0 0 6px 0px var(--clr-primary-strong);
-    color: var(--clr-font-text-inverse);
-    padding: 2rem;
-    z-index: 9999;
-    border-radius: 1.6rem;
-    width: 25ch;
+	article {
+		position: fixed;
+		top: 90px;
+		right: 20px;
+		background-color: var(--clr-primary-main);
+		box-shadow: 0 0 6px 0px var(--clr-primary-strong);
+		color: var(--clr-font-text-inverse);
+		padding: 2rem;
+		z-index: 9999;
+		border-radius: 1.6rem;
+		width: 25ch;
 
-    h6 {
-      font-size: var(--fs-400);
-      font-weight: 400;
-      text-transform: uppercase;
-      letter-spacing: 0.2ch;
-      color: var(--clr-primary-soft-t2);
-      margin: 0;
-      max-width: 20ch;
-      text-align: center;
-    }
-    .subtitle {
-      font-size: var(--fs-200);
-      font-weight: 300;
-      text-transform: uppercase;
-      letter-spacing: 0.5ch;
-      color: var(--clr-primary-soft-t6);
-      margin: 0;
-      max-width: 20ch;
-      text-align: center;
-    }
+		h6 {
+			font-size: var(--fs-400);
+			font-weight: 400;
+			text-transform: uppercase;
+			letter-spacing: 0.2ch;
+			color: var(--clr-primary-soft-t2);
+			margin: 0;
+			max-width: 20ch;
+			text-align: center;
+		}
+		.subtitle {
+			font-size: var(--fs-200);
+			font-weight: 300;
+			text-transform: uppercase;
+			letter-spacing: 0.5ch;
+			color: var(--clr-primary-soft-t6);
+			margin: 0;
+			max-width: 20ch;
+			text-align: center;
+		}
 
-    .logos {
-      position: relative;
+		.logos {
+			position: relative;
 
-      .flow-logo {
-        border-radius: 999rem;
-        width: 120px;
-        padding: 0;
-      }
+			.flow-logo {
+				border-radius: 999rem;
+				width: 120px;
+				padding: 0;
+			}
 
-      .dapp-icon-container {
-        border-radius: 9999px;
-        position: absolute;
-        bottom: -12px;
-        right: -16px;
-        border: 4px var(--clr-primary-main) solid;
-        box-sizing: border-box;
-        box-shadow: 0 0 10px 1px var(--clr-primary-soft-t7);
-        width: 50px;
-        height: 50px;
-      }
-    }
-  }
+			.dapp-icon-container {
+				border-radius: 9999px;
+				position: absolute;
+				bottom: -12px;
+				right: -16px;
+				border: 4px var(--clr-primary-main) solid;
+				box-sizing: border-box;
+				box-shadow: 0 0 10px 1px var(--clr-primary-soft-t7);
+				width: 50px;
+				height: 50px;
+			}
+		}
+	}
 
-  @keyframes pulse {
-    70% {
-      -webkit-transform: scale(1.12) rotate(3deg);
-      box-shadow: 0 0 0 0px var(--clr-primary-soft-t8);
-    }
-    80% {
-      box-shadow: 0 0 1px 3px var(--clr-primary-soft-t9);
-    }
-    100% {
-      -webkit-transform: scale(1) rotate(0deg);
-      box-shadow: 0 0 3px 10px rgba(3, 3, 3, 0);
-    }
-  }
+	@keyframes pulse {
+		70% {
+			-webkit-transform: scale(1.12) rotate(3deg);
+			box-shadow: 0 0 0 0px var(--clr-primary-soft-t8);
+		}
+		80% {
+			box-shadow: 0 0 1px 3px var(--clr-primary-soft-t9);
+		}
+		100% {
+			-webkit-transform: scale(1) rotate(0deg);
+			box-shadow: 0 0 3px 10px rgba(3, 3, 3, 0);
+		}
+	}
 
-  .pulse {
-    animation-name: pulse;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in-out;
-    animation-duration: 1.8s;
-    animation-delay: 1s;
-  }
+	.pulse {
+		animation-name: pulse;
+		animation-iteration-count: infinite;
+		animation-timing-function: ease-in-out;
+		animation-duration: 1.8s;
+		animation-delay: 1s;
+	}
 
-  @keyframes rotate {
-    33% {
-      -webkit-transform: rotate(20deg);
-    }
-    66% {
-      -webkit-transform: rotate(-20deg);
-    }
-    100% {
-      -webkit-transform: rotate(0deg);
-    }
-  }
+	@keyframes rotate {
+		33% {
+			-webkit-transform: rotate(20deg);
+		}
+		66% {
+			-webkit-transform: rotate(-20deg);
+		}
+		100% {
+			-webkit-transform: rotate(0deg);
+		}
+	}
 
-  .rotate {
-    animation-name: rotate;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-out;
-    animation-duration: 3s;
-  }
+	.rotate {
+		animation-name: rotate;
+		animation-iteration-count: infinite;
+		animation-timing-function: ease-out;
+		animation-duration: 3s;
+	}
+
+	.border {
+		border: 2px var(--clr-background-secondary) solid;
+	}
 </style>
