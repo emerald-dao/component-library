18158a9c90bb66e0f658a316b57d72ed2bb3891a
mateoroldos <mateoroldos19@gmail.com>
2022-11-22 17:36:30 -0300

    fix: Workaround to fix storybook deployment

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -11,7 +11,7 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
+		// '@storybook/addon-interactions',
 		'@storybook/addon-svelte-csf',
 		'storybook-addon-theme-switcher'
 	],
