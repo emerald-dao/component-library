2264de69ff756ddf63f2deea4f7655927c6e5623
mateoroldos <mateoroldos19@gmail.com>
2022-12-13 12:36:59 -0300

    chore: New stories

---
+++ src/lib/components/Forms/InputWrapper.stories.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import InputWrapper from './InputWrapper.svelte';
+</script>
+
+<Meta title="Input" component={InputWrapper} />
+
+<Template let:args>
+	<InputWrapper {...args}>
+		<input type="text" name="text-story" />
+	</InputWrapper>
+</Template>
+
+<Story
+	name="Valid text"
+	args={{
+		name: 'text-story',
+		label: 'Text Input',
+		errors: [],
+		isValid: true
+	}}
+/>
+<Story
+	name="Invalid text"
+	args={{
+		name: 'text-story',
+		label: 'Text Input',
+		errors: ['Text should be nicer'],
+		isValid: false
+	}}
+/>
+<Story
+	name="Loading text"
+	args={{
+		name: 'text-story',
+		label: 'Text Input',
+		pending: true,
+		pendingMessage: ['Checking if name exists on the Flow Blockchain'],
+		errors: [],
+		isValid: false
+	}}
+/>
--- src/lib/components/Label/Label.stories.svelte
+++ src/lib/components/Label/Label.stories.svelte
@@ -9,11 +9,50 @@
 	<Label {...args}>Emerald City</Label>
 </Template>
 
-<!-- Primary -->
 <Story
 	name="Primary"
 	args={{
 		color: 'primary',
+		iconLeft: 'tabler:diamond',
+		iconRight: 'tabler:diamond',
+		state: 'on',
+		size: 'small'
+	}}
+/>
+<Story
+	name="Secondary"
+	args={{
+		color: 'secondary',
+		iconLeft: 'tabler:diamond',
+		iconRight: 'tabler:diamond',
+		state: 'on',
+		size: 'small'
+	}}
+/>
+<Story
+	name="Tertiary"
+	args={{
+		color: 'tertiary',
+		iconLeft: 'tabler:diamond',
+		iconRight: 'tabler:diamond',
+		state: 'on',
+		size: 'small'
+	}}
+/>
+<Story
+	name="Transparent"
+	args={{
+		color: 'transparent',
+		iconLeft: 'tabler:diamond',
+		iconRight: 'tabler:diamond',
+		state: 'on',
+		size: 'small'
+	}}
+/>
+<Story
+	name="Neutral"
+	args={{
+		color: 'neutral',
 		iconLeft: 'tabler:diamond',
 		iconRight: 'tabler:diamond',
 		state: 'on',
