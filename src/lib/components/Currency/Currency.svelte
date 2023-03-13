2c2029a34685fe845fc0247077ec23e119255200
mateoroldos <mateoroldos19@gmail.com>
2023-03-13 17:49:00 -0300

    feat: Make money prefix optional in Currency component

--- src/lib/components/Currency/Currency.svelte
+++ src/lib/components/Currency/Currency.svelte
@@ -3,11 +3,12 @@
 	export let currency: 'FLOW' | 'FUSD' | 'USD' | string;
 	export let fontSize: string = '1rem';
 	export let color: 'heading' | 'text' = 'text';
+	export let moneyPrefix = false;
 </script>
 
 <div style={`font-size: ${fontSize}; color: var(--clr-${color}-main)`}>
 	{#if amount >= 0}
-		<span class="amount">{`$${amount.toLocaleString()}`}</span>
+		<span class="amount">{`${moneyPrefix ? '$' : ''}${amount.toLocaleString()}`}</span>
 	{:else}
 		<span class="amount">{`-$${Math.abs(amount).toLocaleString()}`}</span>
 	{/if}
