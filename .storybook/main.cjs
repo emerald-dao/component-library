24c2bdcb20562126d60bbe405ec8dfaf88b8d45f
mateoroldos <mateoroldos19@gmail.com>
2023-05-03 09:32:49 -0300

    fix: Storybook build
    BREAKING CHANGE: Many dependency updates. This can break the GH actions build

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,6 @@
 const sveltePreprocess = require('svelte-preprocess');
 const path = require('path');
 const { loadConfigFromFile, mergeConfig } = require('vite');
-
 module.exports = {
 	stories: [
 		'../src/**/*.stories.mdx',
@@ -15,31 +14,13 @@ module.exports = {
 		'@storybook/addon-svelte-csf',
 		'storybook-addon-theme-switcher'
 	],
-	framework: '@storybook/svelte',
-	core: {
-		builder: '@storybook/builder-vite'
-	},
+	framework: '@storybook/sveltekit',
 	base: './',
-	svelteOptions: {
-		// preprocess: sveltePreprocess()
-	},
 	features: {
 		storyStoreV7: false
 	},
 	staticDirs: ['../static'],
-	async viteFinal(config, { configType }) {
-		const { config: userConfig } = await loadConfigFromFile(
-			path.resolve(__dirname, '../vite.config.js')
-		);
-		// Remove Svelte plugins that would duplicate those added by the Storybook plugin
-		const plugins = userConfig.plugins
-			.flat(1)
-			.filter(
-				(p) => !p.name.startsWith('vite-plugin-svelte') || p.name === 'vite-plugin-svelte-kit'
-			);
-		return mergeConfig(config, {
-			...userConfig,
-			plugins
-		});
+	docs: {
+		docsPage: 'automatic'
 	}
 };
