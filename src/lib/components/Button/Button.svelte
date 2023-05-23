087683682253ee75ddf35f5c8bd9be525893e7fd
mateoroldos <mateoroldos19@gmail.com>
2023-05-23 09:22:38 -0300

    feat: Disable buttons when state = "disabled"

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -53,6 +53,7 @@
 		{title}
 		{form}
 		class={`${state} ${color} ${type} ${size}  width-${width}`}
+		disabled={state === 'disabled'}
 	>
 		{#if statusIconsPosition === 'left'}
 			{#if state === 'loading'}
